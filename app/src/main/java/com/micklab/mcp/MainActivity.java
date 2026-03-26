1403c6ecb89da2f2f5a434fac4dbda5db814545d
mmkurodan <mmkuroda@gmail.com>
2026-03-26 20:37:06 +0000

    Initial commit

---
+++ app/src/main/java/com/micklab/mcp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.micklab.mcp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello from mcp!");
+        tv.setTextSize(24);
+
+        setContentView(tv);
+    }
+}
